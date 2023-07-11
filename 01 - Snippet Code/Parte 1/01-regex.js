7cf9aed3c6cc5f537e3f59296015ac15a4f15321
leandrofiore3 <leandrofiore3@gmail.com>
2023-07-11 16:00:27 -0300

    ejercicio 1 realizado

--- 01 - Snippet Code/Parte 1/01-regex.js
+++ 01 - Snippet Code/Parte 1/01-regex.js
@@ -4,9 +4,10 @@
 //  - Al menos 1 número
 //  - Al menos 1 carácter especial
 
-function validarContraseña() {
-   let tuRegex; // Agrega tu regex
-   return tuRegex;
-}
 
+function validarContraseña() {
+   let tuRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+   return tuRegex;
+ }
+ 
 module.exports = validarContraseña;
