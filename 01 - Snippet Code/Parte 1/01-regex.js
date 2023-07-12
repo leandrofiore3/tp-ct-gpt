bf3cafee64fe99737a834d7d3038eebe9b9c60e5
leandrofiore3 <leandrofiore3@gmail.com>
2023-07-12 14:26:24 -0300

    realizado

--- 01 - Snippet Code/Parte 1/01-regex.js
+++ 01 - Snippet Code/Parte 1/01-regex.js
@@ -1,4 +1,4 @@
-// Este regex debe validar que una contraseña tenga:
+// Este regex (expresion regular) debe validar que una contraseña tenga:
 //  - Al menos 8 caracteres
 //  - Al menos 1 letra mayúscula y 1 minúscula
 //  - Al menos 1 número
@@ -8,6 +8,6 @@
 function validarContraseña() {
    let tuRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
    return tuRegex;
- }
+}
  
 module.exports = validarContraseña;
--- 03 - Testing/01 - Vainilla/tests/02.test.js
+++ 03 - Testing/01 - Vainilla/tests/02.test.js
@@ -1,5 +1,39 @@
 const obtenerPosteo = require('../Ejercicio 02');
 
+jest.mock('axios');
 describe('02 - AsyncRequest', () => {
    // Aquí van tus tests
+   
+      test('Si la función recibe un parámetro que no sea un número del 1 al 100, entonces debe devolver un status 404 y el valor null', async () => {
+        const postID = 101; // Valor fuera del rango esperado
+        const expectedError = '404 Not Found';
+    
+        axios.mockRejectedValueOnce({ response: { status: 404, data: expectedError } });
+    
+        const post = await obtenerPosteo(postID);
+    
+        expect(post).toBeNull();
+        expect(axios).toHaveBeenCalledWith(`https://jsonplaceholder.typicode.com/posts/${postID}`);
+        expect(console.error).toHaveBeenCalledWith(404, expectedError);
+      });
+    
+      test('Si la función recibe un número del 1 al 100, entonces debe devolver un status 200 y un objeto con las propiedades userID, id, title y body', async () => {
+        const postID = 1; // Valor dentro del rango esperado
+        const expectedPost = {
+          userID: 1,
+          id: 1,
+          title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+          body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit' +
+            ' molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        };
+    
+        axios.mockResolvedValueOnce({ data: expectedPost });
+    
+        const post = await obtenerPosteo(postID);
+    
+        expect(post).toEqual(expectedPost);
+        expect(axios).toHaveBeenCalledWith(`https://jsonplaceholder.typicode.com/posts/${postID}`);
+        expect(console.error).not.toHaveBeenCalled();
+      });
+   
 });
